dba798b43bc92bc9914fe00601d0abf0ea68805d
samuelevi87 <samuel.levi.alves@outlook.com>
2020-09-15 03:36:49 -0300

    Project Created

---
+++ backend/src/test/java/com/devsuperior/dspesquisa/DspesquisaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.devsuperior.dspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+ 
